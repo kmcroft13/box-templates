93c6939679fed7d49f38889c268e7ecbe24522b4
Kyle Croft <k.croft@me.com>
2016-03-13 17:16:21 -0500

    Updated security policy to allow Box webapp integration to put app in a frame

--- server/config/security.js
+++ server/config/security.js
@@ -1,7 +1,9 @@
-BrowserPolicy.content.allowOriginForAll("*.googleapis.com");
-BrowserPolicy.content.allowOriginForAll("*.gstatic.com");
-BrowserPolicy.content.allowOriginForAll("*.bootstrapcdn.com");
-BrowserPolicy.content.allowOriginForAll("*.box.com");
-BrowserPolicy.content.allowOriginForAll("*.boxcdn.net");
+BrowserPolicy.content.allowOriginForAll('*.googleapis.com');
+BrowserPolicy.content.allowOriginForAll('*.gstatic.com');
+BrowserPolicy.content.allowOriginForAll('*.bootstrapcdn.com');
+BrowserPolicy.content.allowOriginForAll('*.box.com');
+BrowserPolicy.content.allowOriginForAll('*.boxcdn.net');
 
 BrowserPolicy.content.allowFontDataUrl();
+
+BrowserPolicy.framing.restrictToOrigin('*.box.com');
