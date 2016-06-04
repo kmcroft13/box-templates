5017ec8c4f479ca637f3ad3e25cfca44cd2d4d2c
Kyle Croft <k.croft@me.com>
2016-06-04 10:13:18 -0500

    Corrected Analytics user tracking code

--- client/lib/analytics.js
+++ client/lib/analytics.js
@@ -1,5 +1,9 @@
 GARecordPage = function(pageLocation) {
   ga('create', 'UA-78647216-1', 'auto');
+  if (Meteor.userId()) {
+    var user = Meteor.userId();
+    ga('set', 'userId', user); // Set the user ID using signed-in user_id
+  }
   ga('send', 'pageview', {
     page: pageLocation
   });
