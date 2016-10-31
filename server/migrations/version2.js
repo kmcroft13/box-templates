ea75af47b9f41e440d5c68d777bcfa1ed380c202
Kyle Croft <k.croft@me.com>
2016-10-30 23:46:03 -0500

    Added multi param to update all documents

--- server/migrations/version2.js
+++ server/migrations/version2.js
@@ -3,20 +3,28 @@ Migrations.add({
   name: 'Add dynamicRename and sharing section to existing Templates',
   up: function() {
       //code to migrate up to version 2
-        Template.update({ "dynamicRename": {$in: [null, false]} }, {
+        const result = Template.update({ "dynamicRename": {$in: [null, false]} }, {
             $set: {
                 dynamicRename: { isUsing: false},
                 sharing: { shared: false }
             }
-        });
+        },
+        { multi: true }
+        );
+
+        console.log(result);
       },
   down: function() {
       //code to migrate down to version 1
-        Template.update({ $or: [ "dynamicRename": {$exists: true}, "sharing": {$exists: true} ] }, {
+        const result = Template.update({ $or: [ "dynamicRename": {$exists: true}, "sharing": {$exists: true} ] }, {
             $unset: {
                 dynamicRename: "",
                 sharing: ""
             }
-        });
+        },
+        { multi: true }
+        );
+
+        console.log(result);
   }
 });
