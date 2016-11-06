d97673945db05e7b5eb6eae682e07735e348aa32
Kyle Croft <k.croft@me.com>
2016-11-06 14:49:26 -0600

    Support for Shared Templates (Beta)

--- server/methods/templates/addTemplate.js
+++ server/methods/templates/addTemplate.js
@@ -1,12 +1,28 @@
 Meteor.methods({
 
-  'addTemplate'(templateName, templateDescription, items, usesDynamicRename, findValues) {
+  'addTemplate'(templateName, templateDescription, items, usesSharing, usesDynamicRename, findValues) {
     console.log("### ADDING TEMPLATE ###");
     check(templateName, Match.Any);
     check(templateDescription, Match.Any);
     check(items, Match.Any);
+    check(usesSharing, Match.Any);
     check(usesDynamicRename, Match.Any);
     check(findValues, Match.Any);
+
+    let sharingObj;
+    let dynamicRenameObj;
+    const boxRole = Meteor.user().profile.boxRole;
+
+    if (usesSharing == true && (boxRole == "admin" || boxRole == "coadmin")) {
+      sharingObj = {
+        shared: true,
+        eid: Meteor.user().profile.eid
+      }
+    } else {
+      sharingObj = {
+        shared: false
+      }
+    }
 
     if (usesDynamicRename == true) {
       dynamicRenameObj = {
@@ -27,9 +43,7 @@ Meteor.methods({
       description: templateDescription,
       items: items,
       dynamicRename: dynamicRenameObj,
-      sharing: {
-          shared: false
-      },
+      sharing: sharingObj,
     });
 
     console.log(JSON.stringify({
