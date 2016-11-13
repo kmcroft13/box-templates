605a7ad4f67a778441fde607975cb7668981bc87
Kyle Croft <k.croft@me.com>
2016-11-12 21:09:00 -0600

    Fix for Safari strict CSP for websockets issue

--- server/config/security.js
+++ server/config/security.js
@@ -3,9 +3,13 @@ BrowserPolicy.content.allowOriginForAll('*.gstatic.com');
 BrowserPolicy.content.allowOriginForAll('*.bootstrapcdn.com');
 BrowserPolicy.content.allowOriginForAll('*.box.com');
 BrowserPolicy.content.allowOriginForAll('*.boxcdn.net');
+BrowserPolicy.content.allowOriginForAll("www.google-analytics.com");
 
 BrowserPolicy.content.allowFontDataUrl();
 
 BrowserPolicy.framing.restrictToOrigin('*.box.com');
 
-BrowserPolicy.content.allowOriginForAll("www.google-analytics.com");
+//Fix for Safari CSP being too strict
+BrowserPolicy.content.allowConnectOrigin("ws://localhost:*");
+BrowserPolicy.content.allowConnectOrigin("wss://cloudtemplates-stage.herokuapp.com:*");
+BrowserPolicy.content.allowConnectOrigin("wss://cloudtemplates-prod.herokuapp.com:*");
