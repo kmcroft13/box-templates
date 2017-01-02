ce68ad80d27370793a1f196a9264509850d52b31
Kyle Croft <k.croft@me.com>
2017-01-02 13:09:50 -0600

    Dynamically generate cert code for cert Endpoint

--- routes/cert.js
+++ routes/cert.js
@@ -1,14 +1,14 @@
 Router.map(function () {
   this.route('certEndpoint', {
-    path: '/.well-known/acme-challenge/UEuc55cXcy1S6DGwyqT1CmdRXwoFpz9EtLfk_B5e8-s',
+    path: '/.well-known/acme-challenge/:certId',
     where: 'server', 
     action: function () {
         // "this" is the RouteController instance.
         // "this.response" is the Connect response object
         // "this.request" is the Connect request object
-
+        certId = this.params.certId
         this.response.setHeader('Content-Type', 'text/html');
-        this.response.end("UEuc55cXcy1S6DGwyqT1CmdRXwoFpz9EtLfk_B5e8-s.zllO-r-xGjmlbTlLmzqFxTduhfyhQ609j_3Y-dzyW9I");
+        this.response.end(certId + ".zllO-r-xGjmlbTlLmzqFxTduhfyhQ609j_3Y-dzyW9I");
     },
 
   });
