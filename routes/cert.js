f9783ba75df32bc4c71dab58c2bf8048eaf2b4c5
Kyle Croft <k.croft@me.com>
2017-01-02 11:19:53 -0600

    Added certificate route

---
+++ routes/cert.js
@@ -0,0 +1,7 @@
+Router.route('/.well-known/acme-challenge/UEuc55cXcy1S6DGwyqT1CmdRXwoFpz9EtLfk_B5e8-s', function () {
+
+    this.response.end("UEuc55cXcy1S6DGwyqT1CmdRXwoFpz9EtLfk_B5e8-s.zllO-r-xGjmlbTlLmzqFxTduhfyhQ609j_3Y-dzyW9I");
+
+}
+, {where: 'server'}
+);
