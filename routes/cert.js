bc9ee973cb007093e583d8224d387143fdc95308
Kyle Croft <k.croft@me.com>
2017-01-02 11:44:45 -0600

    Updated cert Endpoint

--- routes/cert.js
+++ routes/cert.js
@@ -1,7 +1,15 @@
-Router.route('/.well-known/acme-challenge/UEuc55cXcy1S6DGwyqT1CmdRXwoFpz9EtLfk_B5e8-s', function () {
+Router.map(function () {
+  this.route('certEndpoint', {
+    path: '/.well-known/acme-challenge/UEuc55cXcy1S6DGwyqT1CmdRXwoFpz9EtLfk_B5e8-s',
+    where: 'server', 
+    action: function () {
+        // "this" is the RouteController instance.
+        // "this.response" is the Connect response object
+        // "this.request" is the Connect request object
 
-    this.response.end("UEuc55cXcy1S6DGwyqT1CmdRXwoFpz9EtLfk_B5e8-s.zllO-r-xGjmlbTlLmzqFxTduhfyhQ609j_3Y-dzyW9I");
+        this.response.setHeader('Content-Type', 'text/html');
+        this.response.end("UEuc55cXcy1S6DGwyqT1CmdRXwoFpz9EtLfk_B5e8-s.zllO-r-xGjmlbTlLmzqFxTduhfyhQ609j_3Y-dzyW9I");
+    },
 
-}
-, {where: 'server'}
-);
+  });
+});
